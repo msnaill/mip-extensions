b9dc96505ebad1d95beea98dfbc9be844338e4c6
msnaill <576604471@qq.com>
2017-11-21 16:41:25 +0800

    change

--- mip-yesky-download/mip-yesky-download.js
+++ mip-yesky-download/mip-yesky-download.js
@@ -5,22 +5,22 @@
 * @version 1.0.0
 */
 define(function (require) {
-    var $ = require('jquery');
+    var $ = require('zepto');
     var customElem = require('customElement').create();
-    customElem.prototype.build = function () {
-        var ar = document.createElement('script');
-        ar.src = 'http://m.yesky.com/TLimages2009/yesky/js/jQuery.md5.js';
-        ar.type = 'text/javascript';
-        document.body.appendChild(ar);
-        ar.onload = function () {
-            $('mip-myesky-down').find('.down').click(function () {
-                var filepath = $(this).attr('data-url');
-                var hexTime = '' + new Number(Math.floor(new Date().getTime() / 1000)).toString(16);
-                var md5 = jQuery.md5('yesky_download' + filepath + hexTime);
-                var newlink = 'http://cdn1.mydown.yesky.com/' + hexTime + '/' + md5 + filepath;
-                $(this).attr('href', newlink);
+    customElem.prototype.firstInviewCallback = function () {
+        require(['http://m.yesky.com/TLimages2009/yesky/js/jQuery.md5'], function () {
+            var element = this.element;
+            var referId = $(element).attr('data-id');
+            var filepath = $(element).attr('data-url');
+            var hexTime = Math.floor(new Date().getTime() / 1000).toString(16);
+            var md5 = jQuery.md5('yesky_download' + filepath + hexTime);
+            var newlink = 'http://cdn1.mydown.yesky.com/' + hexTime + '/' + md5 + filepath;
+            $(element).find('.down').attr('href', newlink);
+            $(element).click(function () {
+                var url = 'http://more.tianjimedia.com/soft/mdown.jsp?id=' + referId + '&f=1&rnd=' + Date.parse(new Date());
+                $.getScript(url);
             });
-        };
+        });
     };
     return customElem;
 });
